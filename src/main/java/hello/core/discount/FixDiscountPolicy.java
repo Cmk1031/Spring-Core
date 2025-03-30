6f3a45923e726217f00649bde0c8e0dfccefc1c9
Cmk1031 <maple1031@naver.com>
2025-03-30 14:59:59 +0900

    조회 빈이 2개 이상 - 문제

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FixDiscountPolicy implements DiscountPolicy{
-
     private int discountFixAmount = 1000;
-
     @Override
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP) return discountFixAmount;
